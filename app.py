bddcc39a7a31aa521713d66144ac6b0d1e179c7c
LargerJoker <diego98vg@gmail.com>
2025-09-14 21:11:08 -0600

    diagrama de dispersion anadido

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly_express as px
 
 car_data = pd.read_csv("vehicles_us.csv")
 
-st.header('Construir un histograma para el conjunto de datos de anuncio de venat de coches')
+st.header('Construir un histograma para el conjunto de datos de anuncio de ventas de coches')
 hist_checkbox = st.checkbox('Construir histograma') # craer una casilla de verificacion
 
 if hist_checkbox: #al hacer click en el boton
@@ -16,5 +16,19 @@ if hist_checkbox: #al hacer click en el boton
 
     #mostrar grafico plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+st.header('Construir un diagrama de dispersion para el conjunto de datos de anuncio de ventas de coches')
+scatter_checkbox = st.checkbox('Construir diagram de dispersion')
+
+if scatter_checkbox:
+    #escribir mensaje
+    st.write('Creacion de diagrama de disperison para el conjunto de datos de anuncios de ventas de coche')
+
+    #crear diagrama de dipserison
+    scatter = px.scatter(car_data, x ='model_year', y ='price')
+
+    #mostrar grafico de plotly interactivo
+    st.plotly_chart(scatter, use_container_width=True)
+
 
 
